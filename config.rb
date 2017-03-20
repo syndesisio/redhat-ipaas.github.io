18c04bf42db175f3f6d14f77ff39e98e4a248b85
kahboom <rachelyordan@gmail.com>
2017-03-20 19:12:07 -0400

    Update circle.yml

--- config.rb
+++ config.rb
@@ -50,3 +50,8 @@ end
 # Deploy Configuration
 # If you want Middleman to listen on a different port, you can set that below
 set :port, 4567
+
+activate :deploy do |deploy|
+  deploy.deploy_method = :git
+  deploy.branch = 'gh-pages'
+end
