281cca48e31ddf8996220235082b384a0450d04e
kahboom <rachelyordan@gmail.com>
2017-04-03 21:21:46 -0400

    Update config.rb

--- config.rb
+++ config.rb
@@ -65,5 +65,5 @@ activate :deploy do |deploy|
   # deploy.remote   = 'custom-remote' # remote name or git url, default: origin
   deploy.branch   = 'master' # default: gh-pages
   # deploy.strategy = :submodule      # commit strategy: can be :force_push or :submodule, default: :force_push
-  # deploy.commit_message = 'custom-message'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
+  deploy.commit_message = 'Deploy to GitHub'      # commit message (can be empty), default: Automated commit at `timestamp` by middleman-deploy `version`
 end
